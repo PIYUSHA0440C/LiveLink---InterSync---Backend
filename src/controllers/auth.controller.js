1ca4f5060819c8794ac696ebbf27d4bc9cec6e03
Piyush Shrivastav <piyushshrivastav106@gmail.com>
2026-04-07 08:43:51 +0530

    Initial commit - Initial setup and MongoDB setup done.

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export async function signup(req, res){
+    res.send("Signup Router")
+}
+
+export async function login(req, res){
+    res.send("Login Router")
+}
+
+export function logout(req, res){
+    res.send("Logout Router")
+}
