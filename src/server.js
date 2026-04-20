e0f18baf5e4e5b04f7882830b71bed6d5167b9e4
Piyush Shrivastav <piyushshrivastav106@gmail.com>
2026-04-20 20:42:56 +0530

    cors setup done.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -12,6 +13,10 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -19,7 +24,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);  
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
     connectDB();
 })
